b8e8aa5bedb225675f1014a350bbb2c51dd1112b
Jan Handzel <jhandzel130696@gmail.com>
2021-06-17 19:28:43 +0200

    working hero and list component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
         <List />
+        
       </main>
     )
   }
