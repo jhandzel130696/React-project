705e36e456cd2daaca86ada9446112d6299427a0
Jan Handzel <jhandzel130696@gmail.com>
2021-06-21 12:56:25 +0200

    dodanie adresu zdjecia do app.js nie dziala

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
         <List title={['Things to do',<sup key='1'>soon!</sup>]}/>
-        <List photo= 'src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="Universe"'/>  
+        <List photo= {img='src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="Universe"'}/>  
         
         
       </main>
