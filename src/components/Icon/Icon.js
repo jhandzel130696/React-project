71ebd3b49950593de296e0702190b70d5755fabc
Jan Handzel <jhandzel130696@gmail.com>
2021-06-24 19:58:47 +0200

    nie dziala

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
+
+// eslint-disable-next-line react/prop-types
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
 
 export default Icon;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -34,9 +34,9 @@ class List extends React.Component {
             key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
             title,
             icon: 'list-alt',
-            cards: []
-          }
-        ]
+            cards: [],
+          },
+        ],
       }
     ));
   }
@@ -45,22 +45,22 @@ class List extends React.Component {
     return (
       <section className={styles.component}> 
         <Hero titleText={this.props.title}
-              imageImg={this.props.image}/>
+          imageImg={this.props.image}/>
         
         <div className={styles.description}>
-        {ReactHtmlParser(this.props.description)}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-        {this.state.columns.map(({key,title, ...columnProps}) => (
-        <Column key={key} name={title} {...columnProps} />
-              ))}
+          {this.state.columns.map(({key,title, ...columnProps}) => (
+            <Column key={key} name={title} {...columnProps} />
+          ))}
         </div>
         <div className={styles.creator}>
-        <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
         
       </section>
-    )
+    );
   }
 }
 
