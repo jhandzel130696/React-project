e3871e2ae73ddabc1a1c16cc5f40bf4452822d14
Jan Handzel <jhandzel130696@gmail.com>
2021-06-25 11:08:44 +0200

    nie dziala webpack dalej dodanie redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
 
